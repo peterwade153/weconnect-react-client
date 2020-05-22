123f0821082f563211d90c739f523e679e13c04a
peter walugembe <peterwade153@gmail.com>
2020-05-22 19:27:45 +0300

    Update config.js
    Update base url

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //config defaults for instance
 const instance = axios.create({
-    baseURL:"https://weconect-api.herokuapp.com/"
+    baseURL:"https://weconect-api.herokuapp.com/api/v2"
     // baseURL:"http://127.0.0.1:5000/api/v2/"
 });
 
